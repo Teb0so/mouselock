f6fcd2b26ccf64f5ad5d04ef40bc1e1b1327e352
Teb0so <joaopedrostcunha@gmail.com>
2026-07-20 21:00:14 -0300

    fake mouse can click now!

--- mlock.c
+++ mlock.c
@@ -4,12 +4,22 @@
 #include <fcntl.h>
 #include <linux/input.h>
 #include <unistd.h>
-#include <libevdev-1.0/libevdev/libevdev.h>
+#include <libevdev/libevdev.h>
+#include <libevdev/libevdev-uinput.h>
+
+enum actiontype{
+    CLICK,
+    RELEASE
+};
 
 typedef struct {
     int fd;
     struct libevdev *dev;
 } Mouse;
+
+typedef struct {
+    struct libevdev_uinput *udev;
+} FakeMouse;
 
 // TODO: iterate over every device to find mouses with findmouse() function
 int mouse_init(Mouse *mouse, const char *device) {
@@ -24,7 +34,28 @@ int mouse_init(Mouse *mouse, const char *device) {
     return 0;
 }
 
-void event_handler(Mouse *mouse) {
+int fmouse_create(Mouse mouse, FakeMouse *fmouse) {
+    int rc;
+
+    rc = libevdev_uinput_create_from_device(mouse.dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &fmouse->udev);
+    if (rc == 0) return -1;
+
+    return 0;
+}
+
+// actiontype can be CLICK or RELEASE
+void fmouse_action(FakeMouse *fmouse, int actiontype) {
+    if (actiontype == CLICK) {
+        libevdev_uinput_write_event(fmouse->udev, EV_KEY, BTN_LEFT, 1);
+        libevdev_uinput_write_event(fmouse->udev, EV_SYN, SYN_REPORT, 0);
+    }
+    else if (actiontype == RELEASE) {
+        libevdev_uinput_write_event(fmouse->udev, EV_KEY, BTN_LEFT, 0);
+        libevdev_uinput_write_event(fmouse->udev, EV_SYN, SYN_REPORT, 0);
+    }
+}
+
+void event_handler(Mouse *mouse, FakeMouse *fmouse) {
     struct input_event ev;
     int rc;
 
@@ -34,7 +65,11 @@ void event_handler(Mouse *mouse) {
         switch (ev.type) {
             case EV_KEY:
                 if (ev.code == BTN_LEFT) {
-                    if (ev.value) printf("click\n");
+                    if (ev.value) {
+                        printf("click\n");
+                        fmouse_action(fmouse, CLICK);
+                        fmouse_action(fmouse, RELEASE);
+                    }
                     else printf("release\n");
                 }
 
@@ -56,11 +91,13 @@ int mouse_destroy(Mouse *mouse) {
 
 int main() {
     Mouse mouse;
+    FakeMouse fmouse;
 
     mouse_init(&mouse, "/dev/input/event5");
+    fmouse_create(mouse, &fmouse);
 
     while (true) {
-        event_handler(&mouse);
+        event_handler(&mouse, &fmouse);
     }
     mouse_destroy(&mouse);
 }
